7824d1c09a593fb7e88c1668932c179d1cd3b4a9
Sergio Romero <sergiod-romeroc@unilibre.edu.co>
2026-05-11 23:38:55 -0500

    primer commit

---
+++ adserver/src/test/java/com/universidad/adserver/AdserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.adserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
